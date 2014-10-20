1d331eddafee0e1b8db5da802b55e22dd92c5c74
Marc Junyent Martín <marc.juny@gmail.com>
2014-10-20 10:03:16 +0200

    Initial Commit

---
+++ Synesthesia/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Synesthesia
+//
+//  Created by Marc Junyent Martín on 20/10/14.
+//  Copyright (c) 2014 Marc Junyent Martín. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
